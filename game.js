28755ffa11b342336712c9bc564b06d3c4d82fe2
PSC Student <77242772+Royal-v@users.noreply.github.com>
2022-10-26 11:20:07 -0400

    game update!!! 1.0

--- game.js
+++ game.js
@@ -13,7 +13,14 @@ function randomizeChicken(){
     chicken.style.top = random(game.offsetHeight - chicken.offsetHeight) + "px";
     chicken.style.left = random(game.offsetWidth - chicken.offsetWidth) + "px";
 }
-
+function randomizecow(){
+    cow.style.top = random(game.offsetHeight - cow.offsetHeight) + "px";
+    cow.style.left = random(game.offsetWidth - cow.offsetWidth) + "px";
+}
+function randomizepig(){
+    pig.style.top = random(game.offsetHeight - cow.offsetHeight) + "px";
+    pig.style.left = random(game.offsetWidth - cow.offsetWidth) + "px";
+}
 //Call this function when the player clicks the chicken
 function youWin(){
     alert("Winner!");
@@ -22,7 +29,8 @@ function youWin(){
 //We've seen this before. Call the "youWin" function
 //whenever the user clicks the chicken
 chicken.addEventListener('click', youWin);
-
+cow.addEventListener('click', youWin);
+pig.addEventListener('click', youWin)
 //A new event lisener! "mouseover" happens when the MOUSE goes
 //OVER the element.
 //
@@ -35,7 +43,22 @@ chicken.addEventListener('mouseover', function(){
         randomizeChicken();
     }
 });
-
+cow.addEventListener('mouseover', function(){
+    //When you get the mouse over the chicken there
+    //is an 80% chance the chicken moves before you
+    //can click it
+    if ( random(100) < 60 ){
+        randomizecow();
+    }
+});
+pig.addEventListener('mouseover', function(){
+    if ( random(100) < 90){
+        randomizepig();
+    }
+    
+});
 //Another function the browser gives us!
 //Every 1000 miliseconds (1 second) move that bird
 window.setInterval(randomizeChicken, 1000);
+window.setInterval(randomizecow, 1200);
+window.setInterval(randomizepig, 600)
